95164eb3d9420f42c2c99a7a5fedcf811281e4d5
suttree <duncan@suttree.com>
2020-08-07 21:12:34 +0100

    First version - creating random symbols in a grid

--- glyphe.py
+++ glyphe.py
@@ -8,18 +8,18 @@ print(icons)
 
 total_width = 700 * 2 + 20 * 3 # two 700px icons plus 20px padding on the left, right and middle
 total_height = total_width # same, we want to make a square
-new_image = Image.new('RGB', (total_width, total_height), (255, 255, 255))
+new_image = Image.new('RGBA', (total_width, total_height), (0, 0, 0))
 
 offset = (20, 20)
 new_image.paste( Image.open('./icons/' + icons[0]), offset )
 
-offset = (new_image.width - 20, 20)
+offset = ( int(new_image.width / 2) + 20, 20 )
 new_image.paste( Image.open('./icons/' + icons[1]), offset )
 
 offset = (20, int(new_image.height / 2) + 20)
 new_image.paste( Image.open('./icons/' + icons[2]), offset )
 
-offset = (new_image.width - 20, int(new_image.height / 2) + 20)
+offset = ( int(new_image.width / 2) + 20, int(new_image.height / 2) + 20 )
 new_image.paste( Image.open('./icons/' + icons[3]), offset )
 
-new_image.save('test.png')
+new_image.save('glyphe.png')
