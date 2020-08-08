572d52a5f2b43191b2483bd5b7988a267bef96bb
suttree <duncan@suttree.com>
2020-08-08 13:47:11 +0100

    Updated filepaths for pi

--- glyphe.py
+++ glyphe.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+dir = '/home/pi/src/glyphe/'
+
 import os, random
 from PIL import Image
-icons = random.sample(os.listdir("./icons/"), 4)
-print(icons)
+icons = random.sample(os.listdir(dir + 'icons/'), 4),
+
 
 total_width = 1947 # to make this 400/300 on resize
 total_height = 1460 # 2 * 700 + 60
@@ -11,21 +13,22 @@ total_height = 1460 # 2 * 700 + 60
 new_image = Image.new('RGBA', (total_width, total_height), (220, 220, 220))
 
 offset = (20, 20)
-new_image.paste( Image.open('./icons/' + icons[0]), offset )
+new_image.paste( Image.open(dir + 'icons/' + icons[0]), offset )
 
 offset = ( int(new_image.width / 2) + 253, 20 )
-new_image.paste( Image.open('./icons/' + icons[1]), offset )
+new_image.paste( Image.open(dir + 'icons/' + icons[1]), offset )
 
 offset = (20, int(new_image.height / 2) + 20)
-new_image.paste( Image.open('./icons/' + icons[2]), offset )
+new_image.paste( Image.open(dir + 'icons/' + icons[2]), offset )
 
 offset = ( int(new_image.width / 2) + 253, int(new_image.height / 2) + 20 )
-new_image.paste( Image.open('./icons/' + icons[3]), offset )
+new_image.paste( Image.open(dir + 'icons/' + icons[3]), offset )
 
 new_image.convert('P')
-new_image.save('glyphe.png')
+new_image.save(dir + 'saved/glyphe.png')
 
-img = Image.open('./glyphe.png')
+# Now resize the image to 400x300 and save it for eink display
+img = Image.open(dir + 'saved/glyphe.png')
 w, h = img.size
 
 h_new = 300
@@ -40,11 +43,6 @@ y0 = 0
 y1 = h_new
 
 img = img.crop((x0, y0, x1, y1))
-
-pal_img = Image.new("P", (1, 1))
-pal_img.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
-
-#img = img.convert("RGB").quantize(palette=pal_img)
 img = img.quantize() 
 
 #from inky import InkyWHAT
@@ -54,4 +52,4 @@ img = img.quantize()
 #inky_display.set_image(img)
 #inky_display.show() 
 
-img.save('glyphe-resized.png')
+img.save(dir + 'saved/glyphe-resized.png')
