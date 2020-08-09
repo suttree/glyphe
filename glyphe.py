35d89c1a499680a8ecb6c0ff15517abe80988c5e
suttree <duncan@suttree.com>
2020-08-09 13:01:21 +0100

    wip for phat screens

--- glyphe.py
+++ glyphe.py
@@ -4,32 +4,31 @@ dir = '/home/pi/src/glyphe/'
 
 import os, random
 from PIL import Image
-icons = random.sample(os.listdir(dir + 'icons/'), 4)
+icons = random.sample(os.listdir(dir + 'icons/'), 3)
 print(icons)
 
-total_width = 1947 # to make this 400/300 on resize
-total_height = 1460 # 2 * 700 + 60
+# The inkyphat dimensions are 212x104
+# We'll stack three 700px in a line to create a 2100x1030 canvas, and resize that down later
+total_width = 2100
+total_height = 1030
 
 new_image = Image.new('RGBA', (total_width, total_height), (255, 255, 255))
 
-offset = ( int(total_width / 2) - 700, 30)
+offset = (0, 0)
 new_image.paste( Image.open(dir + 'icons/' + str(icons[0])), offset )
 
-offset = ( int(total_width / 2), 30 )
+offset = (700, 0)
 new_image.paste( Image.open(dir + 'icons/' + icons[1]), offset )
 
-offset = ( int(total_width / 2) - 700, int(new_image.height / 2) + 10)
+offset = (1400, 0) 
 new_image.paste( Image.open(dir + 'icons/' + icons[2]), offset )
-
-offset = ( int(new_image.width / 2), int(new_image.height / 2) + 10 )
-new_image.paste( Image.open(dir + 'icons/' + icons[3]), offset )
 
 new_image.convert('P')
 new_image.save(dir + 'saved/glyphe.png')
 
 # Now resize the image to 400x300 and save it for eink display
 img = Image.open(dir + 'saved/glyphe.png')
-img = img.resize((400, 300), resample=Image.LANCZOS)
+img = img.resize((212, 104), resample=Image.LANCZOS)
 img = img.quantize() 
 img.save(dir + 'saved/glyphe-resized.png')
 
